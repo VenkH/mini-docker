64db46d8bcc1ddae2294c670130b786c52bf69ac
Venk <3494857305@qq.com>
2024-07-06 09:52:22 +0800

    fix: 将mount namespace设为私有的，避免容器的/proc覆盖主机的/proc导致主机的/proc文件系统不可用 issue：https://github.com/xianlubird/mydocker/issues/41

--- container/init_process_linux.go
+++ container/init_process_linux.go
@@ -17,6 +17,10 @@ import (
 func RunContainerInitProcess(cmd string, args []string) error {
 	log.Infof("command %s", cmd)
 
+	// systemd 加入linux之后, mount namespace 就变成 shared by default, 所以你必须显示
+	// 声明你要这个新的mount namespace独立。
+	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
 	argv := []string{cmd}
